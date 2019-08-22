96f58423eee762b7ba345d4beb1431ca9b35e9d7
Grammy Jiang <grammy.jiang@gmail.com>
2019-08-22 23:22:06 +1000

    Add BaseSettings class and its test case
    This commit adds BaseSettings class and its test case, only __iter__
    method is missing which will be added in next commit

--- minotaur/settings/__init__.py
+++ minotaur/settings/__init__.py
@@ -2,8 +2,24 @@ from __future__ import annotations
 
 import heapq
 import logging
-from collections import namedtuple
-from typing import Any, Dict, List, Optional
+from collections import defaultdict, namedtuple
+from collections.abc import MutableMapping
+from contextlib import contextmanager
+from typing import (
+    KT,
+    VT,
+    Any,
+    Callable,
+    DefaultDict,
+    Dict,
+    Generator,
+    Iterator,
+    List,
+    Mapping,
+    Optional,
+    Tuple,
+    VT_co,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -37,14 +53,68 @@ class SettingAttributes:
         rank: int = get_settings_priority(priority)
         heapq.heappush(self._heap, self.setting_attributes(rank, priority, value))
 
-    def _get(self, property_: str):
+    def _get(self, property_: str) -> Optional[Any]:
         max_values: List = heapq.nlargest(1, self._heap)
-        if max_values:
-            return getattr(max_values[0], property_)
-        return
+
+        return getattr(max_values[0], property_) if max_values else None
 
     def get(self) -> Any:
         return self._get("value")
 
     def get_priority(self) -> Optional[str]:
         return self._get("priority")
+
+
+class BaseSettings(MutableMapping):
+    def __init__(self, settings: Mapping = None, priority="project"):
+        self._frozen: bool = False
+        self._data: DefaultDict[str, SettingAttributes] = defaultdict(SettingAttributes)
+        if settings:
+            self.update(settings, priority)
+        self._frozen = True
+
+    def __contains__(self, key: str) -> bool:
+        return key in self._data
+
+    def __delitem__(self, key: KT) -> None:
+        del self._data[key]
+
+    def __getitem__(self, key: KT) -> VT_co:
+        return self.get(key)
+
+    def __iter__(self) -> Iterator[Tuple[str, Any]]:
+        pass
+
+    def __len__(self) -> int:
+        return len(self._data)
+
+    def __setitem__(self, key: KT, value: VT) -> None:
+        self.set(key, value)
+
+    def get(self, key: KT, default: Any = None) -> Any:
+        value: Any = self._data[key].get()
+        return value if value else default
+
+    def get_priority(self, k: KT) -> Optional[str]:
+        return self._data[k].get_priority()
+
+    def is_frozen(self) -> bool:
+        return self._frozen
+
+    def set(self, key: KT, value: VT, priority: str = "project") -> None:
+        self._data[key].set(value, priority)
+
+    @contextmanager
+    def unfreeze(self) -> Generator[BaseSettings, None, None]:
+        status: bool = self._frozen
+        self._frozen = False
+        try:
+            yield self
+        finally:
+            self._frozen = status
+
+    def update(
+        self, __m: Mapping[KT, VT], priority: str = "project", **kwargs: VT
+    ) -> None:
+        for key, value in __m.items():
+            self._data[key].set(value, priority)
--- tests/settings/test_settings.py
+++ tests/settings/test_settings.py
@@ -1,8 +1,10 @@
+from typing import Dict
 from unittest.case import TestCase
 from unittest.mock import patch
 
 from minotaur.settings import (
     SETTING_PRIORITIES,
+    BaseSettings,
     SettingAttributes,
     get_settings_priority,
 )
@@ -86,3 +88,112 @@ class SettingAttributesTest(TestCase):
         self.assertEqual(
             setting_attributes.get_priority(), self.settings_customize_priority
         )
+
+
+class BaseSettingsTest(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.settings_default: Dict[str, str] = {
+            "test_default_1": "default_1",
+            "test_default_2": "default_2",
+        }
+        cls.settings_user: Dict[str, str] = {
+            "test_user_1": "user_1",
+            "test_user_2": "user_2",
+        }
+        cls.settings_project: Dict[str, str] = {
+            "test_project_1": "project_1",
+            "test_project_2": "project_2",
+        }
+        cls.settings_customize: Dict[str, str] = {
+            "test_customize_1": "customize_1",
+            "test_customize_2": "customize_2",
+        }
+
+    def setUp(self) -> None:
+        self.base_settings = BaseSettings(self.settings_default, priority="default")
+
+    def tearDown(self) -> None:
+        del self.base_settings
+
+    def test_init(self):
+        base_settings = BaseSettings()
+        self.assertDictEqual(base_settings._data, {})
+        self.assertTrue(base_settings._frozen)
+
+        self.assertEqual(len(self.base_settings), len(self.settings_default))
+        for key, value in self.settings_default.items():
+            self.assertEqual(self.base_settings[key], value)
+
+    def test_contains(self):
+        self.assertIn("test_default_1", self.base_settings)
+        self.assertNotIn("test_customize_1", self.base_settings)
+
+    def test_delitem(self):
+        del self.base_settings["test_default_1"]
+        self.assertNotIn("test_default_1", self.base_settings)
+
+    def test_getitem(self):
+        self.assertEqual(self.base_settings["test_default_1"], "default_1")
+
+    def test_iter(self):
+        # TODO: add test
+        pass
+
+    def test_len(self):
+        base_settings = BaseSettings()
+        self.assertEqual(len(base_settings), 0)
+
+        self.assertEqual(len(self.base_settings), len(self.settings_default))
+
+    def test_setitem(self):
+        self.base_settings["test_default_1"] = "project_1"
+        self.assertEqual(self.base_settings["test_default_1"], "project_1")
+        self.assertEqual(self.base_settings.get_priority("test_default_1"), "project")
+
+        self.base_settings["test_default_3"] = "default_3"
+        self.assertEqual(self.base_settings["test_default_3"], "default_3")
+
+        self.assertEqual(self.base_settings.get_priority("test_default_3"), "project")
+
+    def test_get(self):
+        self.assertEqual(self.base_settings.get("test_default_1"), "default_1")
+
+        self.assertNotIn("test_default_3", self.base_settings)
+        self.assertEqual(
+            self.base_settings.get("test_default_3", "default_3"), "default_3"
+        )
+
+    def test_get_priority(self):
+        self.assertEqual(self.base_settings.get_priority("test_default_1"), "default")
+
+        self.assertNotIn("test_default_3", self.base_settings)
+        self.assertIsNone(self.base_settings.get_priority("test_default_3"))
+
+    def test_is_frozen(self):
+        self.assertTrue(self.base_settings.is_frozen())
+
+        self.base_settings._frozen = False
+        self.assertFalse(self.base_settings.is_frozen())
+
+    def test_set(self):
+        self.assertIsNone(self.base_settings.set("test_default_3", "default_3"))
+
+        self.assertEqual(self.base_settings["test_default_3"], "default_3")
+
+    def test_unfreeze(self):
+        self.assertTrue(self.base_settings.is_frozen())
+        with self.base_settings.unfreeze() as base_settings:
+            self.assertIs(base_settings, self.base_settings)
+            self.assertFalse(self.base_settings.is_frozen())
+
+        self.assertTrue(self.base_settings.is_frozen())
+
+    @patch.dict(SETTING_PRIORITIES, {**SETTING_PRIORITIES, "customize": 25})
+    def test_update(self):
+        self.assertIsNone(
+            self.base_settings.update(self.settings_customize, priority="customize")
+        )
+        self.assertIn("test_customize_1", self.base_settings)
+        self.assertIs(self.base_settings["test_customize_1"], "customize_1")
+        self.assertIs(self.base_settings.get_priority("test_customize_1"), "customize")
