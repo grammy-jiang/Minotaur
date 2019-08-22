e9d86b46edeeeb8770d212ff463a3f1aa477484b
Grammy Jiang <grammy.jiang@gmail.com>
2019-08-22 20:09:47 +1000

    Add comments in minotaur.exceptions module

--- minotaur/exceptions.py
+++ minotaur/exceptions.py
@@ -1,2 +1,10 @@
+"""
+Exceptions for this project
+"""
+
+
 class SettingsFrozenException(Exception):
+    """
+    raise this exception when a frozen settings object is trying to be modified
+    """
     pass
