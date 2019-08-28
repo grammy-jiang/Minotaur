1c43026aa41e93470f9c60f621d8d10a2a8aa0f3
Grammy Jiang <gjiang@buildingsalive.com>
2019-08-28 16:47:28 +1000

    Add entry point for command line

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 import versioneer
 
 setup(
+    cmdclass=versioneer.get_cmdclass(),
+    entry_points={"console_scripts": ["minotaur = minotaur.cmdline:execute"]},
     version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass()
 )
